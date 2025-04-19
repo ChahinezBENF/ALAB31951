0fe3e9af015f1aeb8e7b2a43ad5a19cb058e9b2d
ChahinezBENF <Chahinez-benfriha27@outlook.com>
2025-04-18 22:17:32 -0400

    last commit

--- routes/grades.test.js
+++ routes/grades.test.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(express.json());
 app.use('/grades', gradesRouter);
 
+
+//Ensure the database connection is established and cleaned up after all tests
 beforeAll(async () => {
   await mongoose.connect(process.env.ATLAS_URI, {
     dbName: 'test_db', // Use a test database
